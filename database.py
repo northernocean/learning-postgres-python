6925c430541ec1aa7123ea0c938f287897afe140
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-08-10 21:19:39 -0400

    add required sql queries

--- database.py
+++ database.py
@@ -1,3 +1,21 @@
 def create_tables():
     pass
 
+CREATE_MOVIES_TABLE = """
+create table if not exists movies (
+    title TEXT,
+    release_timestamp REAL,
+    watched INTEGER);"""
+
+INSERT_MOVIES = """
+insert into movies (title, release_timestamp, watched) values (?,?,0);"""
+
+SELECT_ALL_MOVIES = """
+select title, release_timestamp, watched from movies;"""
+
+SELECT_UPCOMING_MOVIES = """
+select title, release_timestamp, watched from movies where release_timestamp >= ?;"""
+
+SELECT_WATCHED_MOVIES = """
+select title, release_timestamp, watched from moves where watched = 1;"""
+
