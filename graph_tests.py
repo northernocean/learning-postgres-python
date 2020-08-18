39cf1fe33f3f2ee6c324e5038ca8626e27e888a1
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-08-18 19:56:46 -0400

    using figure and plot objects

--- graph_tests.py
+++ graph_tests.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 
-plt.figure()
-plt.axis([0, 6, 0, 20])  # X axis 0-6 and Y axis 0-20 Note that if we comment this out matplotlib will automatically set axes for us.
-plt.plot([1, 2, 3, 4], [3, 5, 9, 25])
-plt.show()
+figure = plt.figure()
+
+subplot = figure.add_subplot()
+subplot.set_title("Line Graph")
+subplot.set_xlabel("Xs")
+subplot.set_ylabel("Ys")
+subplot.plot([1, 2, 3, 4], [3, 5, 9, 25])
+
+plt.show() # In some context figure.show() will also work here, such in some notebooks.
