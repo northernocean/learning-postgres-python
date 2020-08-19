d2025a9ab8e0e654269c26166a389b47c84b7222
David Pecot <4504352+northernocean@users.noreply.github.com>
2020-08-18 20:17:18 -0400

    pie chart

--- graph_tests.py
+++ graph_tests.py
@@ -1,23 +1,14 @@
 import matplotlib.pyplot as plt
 
+results = [63, 28, 8]
+result_labels = ["flask", "django", "not sure"]
+
 figure = plt.figure()
-p1 = figure.add_subplot(1, 2, 1)  # or add_subplot(121) works
-p2 = figure.add_subplot(1, 2, 2)
-p1.plot([1, 2, 3, 4], [3, 5, 9, 25])
-p2.plot([1, 2, 3, 4], [4, 7, 11, 17])
-plt.show()
-
-
-# other ways to create subplots:
-
-figure, (p1, p2) = plt.subplots(1, 2)  # a figure with two plots side by side
-p1.plot([1, 2, 3, 4], [3, 5, 9, 25])
-p2.plot([1, 2, 3, 4], [4, 7, 11, 17])
-plt.show()
-
-figure, ((p1, p2), (p3, p4)) = plt.subplots(2, 2)  # a figure with four plots 2 x 2
-p1.plot([1, 2, 3, 4], [3, 5, 9, 25])
-p2.plot([1, 2, 3, 4], [4, 7, 11, 17])
-p3.plot([1, 2, 3, 4], [4, 7, 11, 17])
-p4.plot([1, 2, 3, 4], [4, 7, 11, 17])
+p = figure.add_subplot()
+p.pie(
+    results,
+    labels=result_labels,
+    explode=[0.1, 0, 0],
+    autopct="%1.1f%%"
+    )
 plt.show()
